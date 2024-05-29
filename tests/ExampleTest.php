caaff2ba983eec2b92378d618ebaad3f941e18ce
Meet Bhanabhagwan <meetbhana9567@gmail.com>
2024-05-29 16:42:43 +0530

    Github Workflow Revised

---
+++ tests/ExampleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+    /** @test */
+    public function it_checks_if_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
